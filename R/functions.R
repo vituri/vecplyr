eef4b77bea88b265504d34fbbb3378534634c6c5
Guilherme Vituri <56522687+vituri@users.noreply.github.com>
2022-04-19 17:36:17 -0300

    filter e mutate

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,5 @@
 #' @export
-filter.numeric = function(.data, .f, ..., .preserve = FALSE) {
+filter.default = function(.data, .f, ..., .preserve = FALSE) {
   x = .data
   ids = .f(x)
   y = x[ids]
@@ -7,7 +7,44 @@ filter.numeric = function(.data, .f, ..., .preserve = FALSE) {
 }
 
 #' @export
-filter.character = filter.numeric
+mutate.default = function(.tbl, .funs, .predicate = NULL, ...) {
+
+  x = .tbl
+
+  if (is.null(.predicate)) {
+    .predicate = \(x) rep(x = TRUE, times = length(x))
+  }
+
+  ids = .predicate(x)
+
+  if (all(!ids)) {
+    return(x)
+  }
+
+  y = x
+
+  if (is.function(.funs)) {
+    values = .funs(x[ids])
+  } else {
+
+    if (length(.funs) == 1) {
+      .funs = rep(.funs, times = length(ids))
+    }
+
+    values = .funs[ids]
+  }
+
+  y[ids] = values
+
+  return(y)
+}
 
 #' @export
-filter.factor = filter.character
+in_set = function(x, set = x) {
+  x %in% set
+}
+
+#' @export
+in_set_func = function(x, set = x) {
+  \(x) in_set(x, set)
+}
