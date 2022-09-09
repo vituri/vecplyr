5798b37e4113bb7a929bfd11b020d4bb20b20d6c
Guilherme Vituri <56522687+vituri@users.noreply.github.com>
2022-09-09 13:48:06 -0300

    remove generics

--- R/functions.R
+++ R/functions.R
@@ -1,45 +1,3 @@
-# genericas ---------------------------------------------------------------
-#' @export
-filter.default = function(.data, .f, ..., .preserve = FALSE) {
-  x = .data
-  ids = .f(x)
-  y = x[ids]
-  return(y)
-}
-
-#' @export
-mutate.default = function(.tbl, .funs, .predicate = NULL, ...) {
-
-  x = .tbl
-
-  if (is.null(.predicate)) {
-    .predicate = \(x) rep(x = TRUE, times = length(x))
-  }
-
-  ids = .predicate(x)
-
-  if (all(!ids)) {
-    return(x)
-  }
-
-  y = x
-
-  if (is.function(.funs)) {
-    values = .funs(x[ids])
-  } else {
-
-    if (length(.funs) == 1) {
-      .funs = rep(.funs, times = length(ids))
-    }
-
-    values = .funs[ids]
-  }
-
-  y[ids] = values
-
-  return(y)
-}
-
 # novas -------------------------------------------------------------------
 v_to_fun = function(.x) {
   if (is.function(.x)) {
@@ -67,10 +25,6 @@ v_filter = function(.x, .f = v_all_true) {
   y = .x[ids]
   return(y)
 }
-
-.x = 1:10
-.f = c(99, 100) %>% vituripackage::sample_safe(10)
-.p = c(TRUE, FALSE) %>% vituripackage::sample_safe(10)
 
 #' @export
 v_mutate = function(.x, .f = v_all_true, .p = v_all_true) {
--- testes/testes.R
+++ testes/testes.R
@@ -1,3 +1,45 @@
+# genericas ---------------------------------------------------------------
+#' @export
+filter.default = function(.data, .f, ..., .preserve = FALSE) {
+  x = .data
+  ids = .f(x)
+  y = x[ids]
+  return(y)
+}
+
+#' @export
+mutate.default = function(.tbl, .funs, .predicate = NULL, ...) {
+
+  x = .tbl
+
+  if (is.null(.predicate)) {
+    .predicate = \(x) rep(x = TRUE, times = length(x))
+  }
+
+  ids = .predicate(x)
+
+  if (all(!ids)) {
+    return(x)
+  }
+
+  y = x
+
+  if (is.function(.funs)) {
+    values = .funs(x[ids])
+  } else {
+
+    if (length(.funs) == 1) {
+      .funs = rep(.funs, times = length(ids))
+    }
+
+    values = .funs[ids]
+  }
+
+  y[ids] = values
+
+  return(y)
+}
+
 1:10 %>%
   filter(\(x) x >= 4)
 
@@ -61,3 +103,8 @@ tibble(
 
 iris %>%
   as_tibble()
+
+
+.x = 1:10
+.f = c(99, 100) %>% vituripackage::sample_safe(10)
+.p = c(TRUE, FALSE) %>% vituripackage::sample_safe(10)
